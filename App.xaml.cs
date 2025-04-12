075ce51830f17ba45daefd6a8222d893ec261fd3
Student48 <satrker20202@gmail.com>
2025-04-12 10:23:33 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Veterinary_clinic.AddBd;
+
+namespace Veterinary_clinic
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static  Veterinary_clinicEntities Connection = new Veterinary_clinicEntities();
+
+    }
+}
